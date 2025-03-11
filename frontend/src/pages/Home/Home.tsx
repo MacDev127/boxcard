da8e681ec2b896115c512d4a06f04bc5d7228bcf
MacDev <69508380+MacDev127@users.noreply.github.com>
2025-03-11 11:25:20 +0000

    file re structuring

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Navbar from '../../components/Navbar/Navbar';
 import Hero from '../../components/Hero/Hero';
-import Footer from '../../components/Footer/Footer';
+import Footer from '../../components/footer/Footer';
 import './Home.css';
 const Home = () => {
   return (
