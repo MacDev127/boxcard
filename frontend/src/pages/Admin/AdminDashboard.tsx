1044031ce35ca2dc0455b0ba068ba3b13548c175
MacDev <69508380+MacDev127@users.noreply.github.com>
2025-04-02 11:32:50 +0100

    updated admin dashboard layout and styling

--- frontend/src/pages/Admin/AdminDashboard.tsx
+++ frontend/src/pages/Admin/AdminDashboard.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/footer/Footer';
-import './AdminDashboard.css';
+import Navbar from './Navbar'; // adjust path as needed
+import './AdminAddBoxer.css';
 
 interface BoxerFormData {
   name: string;
@@ -57,7 +56,6 @@ const AdminAddBoxer: React.FC = () => {
     setError(null);
     setSuccessMessage(null);
 
-    // Convert numeric values from strings to numbers
     const payload = {
       ...formData,
       age: Number(formData.age),
@@ -67,12 +65,8 @@ const AdminAddBoxer: React.FC = () => {
     };
 
     try {
-      const response = await axios.post(
-        'http://localhost:5002/api/boxers',
-        payload
-      );
+      await axios.post('http://localhost:5002/api/boxers', payload);
       setSuccessMessage('Boxer added successfully!');
-      // Reset the form fields
       setFormData({
         name: '',
         country: '',
@@ -97,181 +91,172 @@ const AdminAddBoxer: React.FC = () => {
   };
 
   return (
-    <>
-      <div className="dashboard">
-        <Navbar />
-        <div className="admin">
-          <h2>Add New Boxer</h2>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
+    <div className="dashboard">
+      <Navbar />
+      <div className="admin">
+        <h2>Add New Boxer</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form__wrapper">
+            <div className="form-column">
+              <div className="form__input">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="country">Country:</label>
+                <input
+                  type="text"
+                  id="country"
+                  name="country"
+                  value={formData.country}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="sex">Sex:</label>
+                <select
+                  id="sex"
+                  name="sex"
+                  value={formData.sex}
+                  onChange={handleChange}
+                >
+                  <option value="">Select Sex</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+              </div>
+              <div className="form__input">
+                <label htmlFor="profileImage">Profile Image URL:</label>
+                <input
+                  type="text"
+                  id="profileImage"
+                  name="profileImage"
+                  value={formData.profileImage}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="club">Club:</label>
+                <input
+                  type="text"
+                  id="club"
+                  name="club"
+                  value={formData.club}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="province">Province:</label>
+                <input
+                  type="text"
+                  id="province"
+                  name="province"
+                  value={formData.province}
+                  onChange={handleChange}
+                />
+              </div>
             </div>
-
-            <div>
-              <label htmlFor="country">Country:</label>
-              <input
-                type="text"
-                id="country"
-                name="country"
-                value={formData.country}
-                onChange={handleChange}
-                required
-              />
+            <div className="form-column">
+              <div className="form__input">
+                <label htmlFor="weight">Weight (KG):</label>
+                <input
+                  type="number"
+                  id="weight"
+                  name="weight"
+                  value={formData.weight}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="stance">Stance:</label>
+                <input
+                  type="text"
+                  id="stance"
+                  name="stance"
+                  value={formData.stance}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="level">Level:</label>
+                <select
+                  id="level"
+                  name="level"
+                  value={formData.level}
+                  onChange={handleChange}
+                  required
+                >
+                  <option value="">Select Level</option>
+                  <option value="schoolboy">Schoolboy</option>
+                  <option value="schoolgirl">Schoolgirl</option>
+                  <option value="youth">Youth</option>
+                  <option value="junior">Junior</option>
+                  <option value="elite">Elite</option>
+                </select>
+              </div>
+              <div className="form__input">
+                <label htmlFor="fightsWon">Fights Won:</label>
+                <input
+                  type="number"
+                  id="fightsWon"
+                  name="fightsWon"
+                  value={formData.fightsWon}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="fightsLost">Fights Lost:</label>
+                <input
+                  type="number"
+                  id="fightsLost"
+                  name="fightsLost"
+                  value={formData.fightsLost}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="videoUrl">Video URL:</label>
+                <input
+                  type="text"
+                  id="videoUrl"
+                  name="videoUrl"
+                  value={formData.videoUrl}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="form__input">
+                <label htmlFor="age">Age:</label>
+                <input
+                  type="number"
+                  id="age"
+                  name="age"
+                  value={formData.age}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
             </div>
-
-            <div>
-              <label htmlFor="sex">Sex:</label>
-              <select
-                id="sex"
-                name="sex"
-                value={formData.sex}
-                onChange={handleChange}
-              >
-                <option value="">Select Sex</option>
-                <option value="Male">Male</option>
-                <option value="Female">Female</option>
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="profileImage">Profile Image URL:</label>
-              <input
-                type="text"
-                id="profileImage"
-                name="profileImage"
-                value={formData.profileImage}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="club">Club:</label>
-              <input
-                type="text"
-                id="club"
-                name="club"
-                value={formData.club}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="province">Province:</label>
-              <input
-                type="text"
-                id="province"
-                name="province"
-                value={formData.province}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="age">Age:</label>
-              <input
-                type="number"
-                id="age"
-                name="age"
-                value={formData.age}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div>
-              <label htmlFor="weight">Weight (KG):</label>
-              <input
-                type="number"
-                id="weight"
-                name="weight"
-                value={formData.weight}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div>
-              <label htmlFor="stance">Stance:</label>
-              <input
-                type="text"
-                id="stance"
-                name="stance"
-                value={formData.stance}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div>
-              <label htmlFor="level">Level:</label>
-              <select
-                id="level"
-                name="level"
-                value={formData.level}
-                onChange={handleChange}
-                required
-              >
-                <option value="">Select Level</option>
-                <option value="schoolboy">Schoolboy</option>
-                <option value="schoolgirl">Schoolgirl</option>
-                <option value="youth">Youth</option>
-                <option value="junior">Junior</option>
-                <option value="elite">Elite</option>
-              </select>
-            </div>
-
-            <div>
-              <label htmlFor="fightsWon">Fights Won:</label>
-              <input
-                type="number"
-                id="fightsWon"
-                name="fightsWon"
-                value={formData.fightsWon}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="fightsLost">Fights Lost:</label>
-              <input
-                type="number"
-                id="fightsLost"
-                name="fightsLost"
-                value={formData.fightsLost}
-                onChange={handleChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="videoUrl">Video URL:</label>
-              <input
-                type="text"
-                id="videoUrl"
-                name="videoUrl"
-                value={formData.videoUrl}
-                onChange={handleChange}
-              />
-            </div>
-
-            {error && <p className="error">{error}</p>}
-            {successMessage && <p className="success">{successMessage}</p>}
-
+          </div>
+          {error && <p className="error">{error}</p>}
+          {successMessage && <p className="success">{successMessage}</p>}
+          <div className="form__button">
             <button type="submit" disabled={isSubmitting}>
               {isSubmitting ? 'Submitting...' : 'Add Boxer'}
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 };
 
